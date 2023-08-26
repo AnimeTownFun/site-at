f759e8008501f75ec81cbcac5d08e55cc736633a
Mohtasim Alam Sohom <94981761+codeblitz97@users.noreply.github.com>
2023-08-26 16:54:53 +0600

    Add cors anywhere

--- src/Components/constants.js
+++ src/Components/constants.js
@@ -1,3 +1,4 @@
-export const HomeApi = 'https://api.consumet.org';
-export const ServerApi = 'https://animetrix-login-backend.vercel.app/api/v1';
-export const StreamApi = 'https://at-api.vercel.app/';
+export const HomeApi = "https://api.consumet.org";
+export const ServerApi = "https://animetrix-login-backend.vercel.app/api/v1";
+export const StreamApi =
+  "https://cors-anywhere.herokuapp.com/https://at-api.vercel.app/";
